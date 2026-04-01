722215b2c338b883f3d42d3b643378d98dfd93d4
7dev <jnpereiraalves@gmail.com>
2026-03-31 22:50:46 -0300

    Initialize Discord bot with client and intents

---
+++ index.js
@@ -0,0 +1,14 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once('ready', () => {
+  console.log(`Bot online como ${client.user.tag}!`);
+});
+
+client.login(process.env.TOKEN);   // O token vem das variáveis de ambiente do Render
